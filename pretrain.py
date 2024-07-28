94b16710ae43f31fbedab0667f072a26d32a8c54
petunder <shkumatov@petya.ru>
2024-07-28 15:58:40 +0500

    Bug fixes

--- pretrain.py
+++ pretrain.py
@@ -5,6 +5,9 @@ from torch.utils.data import DataLoader, Dataset
 from datasets import load_dataset
 from dqn import ChessNetwork
 from chess_common import ChessEnv
+import chess
+import chess.pgn
+import io
 
 
 class ChessDataset(Dataset):
@@ -17,33 +20,59 @@ class ChessDataset(Dataset):
 
     def __getitem__(self, idx):
         game = self.games[idx]
+        moves = game['text'].split()[1::2]  # Получаем только ходы, пропуская номера
+        result = game['Result']
+
         self.env.reset()
-        for move in game['moves'].split():
+        states = []
+        actions = []
+
+        for move in moves:
             state = self.env._get_observation()
-            action = self.env.board.parse_san(move)
-            self.env.step(action.from_square * 64 + action.to_square)
-        return torch.FloatTensor(state), torch.LongTensor([action.from_square * 64 + action.to_square])
+            try:
+                chess_move = self.env.board.parse_san(move)
+                action = chess_move.from_square * 64 + chess_move.to_square
+                self.env.step(action)
+                states.append(state)
+                actions.append(action)
+            except ValueError:
+                print(f"Invalid move: {move}")
+                continue
+
+        # Преобразуем результат в числовое значение
+        if result == "1-0":
+            value = 1
+        elif result == "0-1":
+            value = -1
+        else:  # "1/2-1/2"
+            value = 0
+
+        return torch.stack([torch.FloatTensor(s) for s in states]), torch.LongTensor(actions), torch.FloatTensor(
+            [value] * len(states))
 
 
 def pretrain():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = ChessNetwork().to(device)
     optimizer = optim.Adam(model.parameters(), lr=0.001)
-    criterion = nn.CrossEntropyLoss()
+    policy_criterion = nn.CrossEntropyLoss()
+    value_criterion = nn.MSELoss()
 
-    dataset = load_dataset("LeelaChessZero/chess-games",
-                           split="train[:1000]")  # Загружаем только первые 1000 игр для примера
+    dataset = load_dataset("adamkarvonen/chess_sae_individual_games_filtered",
+                           split="train[:10000]")  # Загружаем первые 10000 игр для примера
     chess_dataset = ChessDataset(dataset)
     dataloader = DataLoader(chess_dataset, batch_size=32, shuffle=True)
 
     num_epochs = 10
     for epoch in range(num_epochs):
         total_loss = 0
-        for states, actions in dataloader:
-            states, actions = states.to(device), actions.to(device)
+        for states, actions, values in dataloader:
+            states, actions, values = states.to(device), actions.to(device), values.to(device)
             optimizer.zero_grad()
-            policy, _ = model(states)
-            loss = criterion(policy, actions.squeeze())
+            policy, value = model(states.view(-1, 13, 8, 8))
+            policy_loss = policy_criterion(policy, actions.view(-1))
+            value_loss = value_criterion(value.squeeze(), values.view(-1))
+            loss = policy_loss + value_loss
             loss.backward()
             optimizer.step()
             total_loss += loss.item()
