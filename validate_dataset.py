9f287b803632372edb91807fccea7387a7c00a2b
petunder <shkumatov@petya.ru>
2024-07-30 12:28:33 +0500

    Bug fixes

--- validate_dataset.py
+++ validate_dataset.py
@@ -28,12 +28,15 @@ def validate_single_game(game_text, game_index=0):
     # Удаление начальной точки с запятой, номеров ходов и точек
     cleaned_moves = re.sub(r'\d+\.', '', game_text.replace(';', '')).split()
 
+    # Удаление элементов, состоящих только из цифр
+    filtered_moves = [move for move in cleaned_moves if not move.isdigit()]
+
     env.reset()  # Сброс доски в начальное положение
 
     print(f"Game {game_index}: {game_text}")
-    print(f"Processed moves: {cleaned_moves}")  # Вывод обработанных ходов для проверки
+    print(f"Processed moves: {filtered_moves}")  # Вывод обработанных ходов для проверки
 
-    for move in cleaned_moves:
+    for move in filtered_moves:
         print(f"Processing move: {move} on board:\n{env.board}")
         try:
             env.push_san(move)
@@ -43,10 +46,12 @@ def validate_single_game(game_text, game_index=0):
 
     print(f"Game {game_index}: Processing complete.")
 
+
 def validate_multiple_games(dataset_name):
     dataset = load_dataset(dataset_name, split="train[:10]")  # Загружаем первые 10 игр
     for i, game in enumerate(dataset):
         validate_single_game(game['text'], game_index=i)
+
 
 dataset_name = "adamkarvonen/chess_sae_individual_games_filtered"
 validate_multiple_games(dataset_name)
